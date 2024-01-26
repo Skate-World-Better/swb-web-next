791ddf162a56f0ff83e65632e5e372f22dc29316
Tadeas Kosek <teddcz@gmail.com>
2024-01-26 02:14:03 +0100

    feat(SWB-007): add ethiopia content

--- src/components/pages/EthiopiaPage/Sections/AboutSection/index.tsx
+++ src/components/pages/EthiopiaPage/Sections/AboutSection/index.tsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row'
 
 import BaseSection from '../../../../BaseSection'
 
-import KidsZambia from '../../../../../images/kids-ethiopia.jpg'
+import KidsEthiopia from '../../../../../images/kids-ethiopia.jpg'
 
 import { Link } from "react-router-dom"
 
@@ -13,20 +13,35 @@ const AboutSection = () => (
     <Row className="mb-4 text-center text-lg-left">
       <Col xs={12} lg={6}>
         <BaseSection.Header.Text className="mb-5">
-          What will we do in <span className="font-alt font-primary">Addis Abeba?</span>
+          What did we do in <span className="font-alt font-primary">Addis Abeba?</span>
         </BaseSection.Header.Text>
         <BaseSection.Text className="mt-lg-2 mr-lg-4 mb-5 mb-md-0">
-          For the first time in our existence we decided to go on a mission to a destination, where skateboarding and skate-related activities already have a long history. The capital city of Ethiopia has two standing skateparks where the localy established organization Ethiopia Skate runs their programs. Based on a recommendation from our friends from <Link to="https://foreverplayground.org">Forever Playground Magazine</Link> we decided to contact Israel Dejene, the local hero who runs a suburban based social project called Megabi House, where all street kids are welcome. He is passionate about skateboarding and he even linked up with Tony Hawk, Nyjah Huston and the gang and they went down there back in 2015 to build him a miniramp right on the land of Megabi House. Unfortunately, we have to take down this miniramp, but we will build a much greater facility by the end of 2023.
-        </BaseSection.Text>
-        <BaseSection.Text className="mt-lg-2 mr-lg-4 mb-5 mb-md-0">
-          "Megabi House is a home for children and youth who literally have no place to sleep. In our community there are many street kids who have no home or family to go home to at the end of the day"
+          For the first time in our existence we decided to go on a mission to a destination,
+          where skateboarding and skate-related activities already have a long history. The
+          capital city of Ethiopia has two standing skateparks where the localy established
+          organization Ethiopia Skate runs their programs. Based on a recommendation from
+          our friends from <Link to="https://foreverplayground.org/">Forever Playground Magazine</Link> we decided to contact Israel Dejene,
+          the local hero who runs a suburban based social project called Megabi House, where
+          all street kids are welcome. He is passionate about skateboarding and he even linked
+          up with Tony Hawk, Nyjah Huston and the gang and they went down there back in
+          2015 to build him a miniramp right on the land of Megabi House. In December 2023
+          we went down to Addis to continue on the job sparked by Tony and we worked out
+          the space into a full skatepark. It took us two weeks of work day and night in a crew
+          of six people. We left a concrete jungle with obstacles for the most skilled riders but
+          also for the very beginners. We decorated the space with a mozaic, couple plants
+          and two palmtrees. Having left more than twenty complete boards, shoes and couple
+          of concrete tools, we believe that this project will continue to thrive in the years to
+          come!
         </BaseSection.Text>
         <BaseSection.Text className="mt-lg-2 mr-lg-4 mb-5 mb-md-0">
           SEE MORE OF HIS WORK <Link to="https://www.megabi.org/megabiskate">HERE!</Link>
         </BaseSection.Text>
       </Col>
       <Col xs={12} lg={6}>
-        <Image src={KidsZambia} fluid rounded />
+        <Image src={KidsEthiopia} fluid rounded className="mt-lg-5" />
+        <BaseSection.Text className="mt-lg-5 mr-lg-4 mb-5 mb-md-0">
+          "Megabi House is a home for children and youth who literally have no place to sleep. In our community there are many street kids who have no home or family to go home to at the end of the day"
+        </BaseSection.Text>
       </Col>
     </Row>
   </BaseSection>
--- src/components/pages/EthiopiaPage/Sections/GallerySection/index.tsx
+++ src/components/pages/EthiopiaPage/Sections/GallerySection/index.tsx
@@ -7,8 +7,13 @@ import ImageCard from '../../../../Card'
 
 import EthiopiaOne from '../../../../../images/ethiopia-1.jpg'
 import EthiopiaTwo from '../../../../../images/ethiopia-2.jpg'
-import EthiopiaThree from '../../../../../images/ethiopia-hero.jpg'
-import EthiopiaFour from '../../../../../images/kids-ethiopia.jpg'
+import EthiopiaThree from '../../../../../images/ethiopia-3.jpg'
+import EthiopiaFour from '../../../../../images/ethiopia-4.jpg'
+import EthiopiaFive from '../../../../../images/ethiopia-5.jpg'
+import EthiopiaSix from '../../../../../images/ethiopia-6.jpg'
+import EthiopiaSeven from '../../../../../images/ethiopia-7.jpg'
+import EthiopiaEight from '../../../../../images/ethiopia-9.jpg'
+import EthiopiaNine from '../../../../../images/ethiopia-8.jpg'
 
 const data = [
   {
@@ -23,12 +28,40 @@ const data = [
   {
     imageSrc: EthiopiaFour,
   },
+  {
+    imageSrc: EthiopiaFive,
+  },
+  {
+    imageSrc: EthiopiaSix,
+  },
+  {
+    imageSrc: EthiopiaSeven,
+  },
+  {
+    imageSrc: EthiopiaEight,
+  },
+  {
+    imageSrc: EthiopiaNine,
+  },
 ]
 
 const GallerySection = () => (
   <BaseSection className="mb-5 py-5 mb-lg-5 py-lg-5">
     <Row className="mb-5 align-items-center py-5 text-center text-lg-left">
-      <Col xs={12} lg={12}>
+      <Col xs={12} lg={6}>
+        <BaseSection.Header.Annotation className="mb-4 sub">
+          Stories
+        </BaseSection.Header.Annotation>
+        <BaseSection.Header.Text className="mb-5">
+          Some cool <span className="font-alt font-primary">moments</span>
+        </BaseSection.Header.Text>
+        <BaseSection.Text className="py-4 mr-lg-5">
+          Here are a few pictures from Ethiopia
+          and the construction process. Should you be interested in having
+          one printed on your wall, just let us know!
+        </BaseSection.Text>
+      </Col>
+      <Col xs={12} lg={6}>
         <MediaCarousel
           items={data}
           config={{
